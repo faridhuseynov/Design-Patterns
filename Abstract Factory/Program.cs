d499ea606dc36848b49b71824699c4f9f972c9c4
Farid Huseynov <F.H189@hotmail.com>
2018-10-08 01:28:28 +0400

    Added class Continents - all looks done!

--- Abstract Factory/Program.cs
+++ Abstract Factory/Program.cs
@@ -4,80 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Abstract_Factory.Animals;
+using static Abstract_Factory.Continents;
 
 namespace Abstract_Factory
 {
-    public abstract class Continent
-    {
-        public List<Herbivore> herbivores;
-        public List<Carnivore> carnivores;
-        public abstract Carnivore CreateCarnivore();
-        public abstract Herbivore CreateHerbivore();
-    }
-    // Africa is the Factory Class, which should has its own flora and fauna
-    public class Africa : Continent
-    {
-        //constructor to initialize list of carnivores and herbivores
-        public Africa()
-        {
-            herbivores = new List<Herbivore>();
-            carnivores = new List<Carnivore>();
-        }
-        //method to create Lion in Africa
-        public  override Carnivore CreateCarnivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection%2==0)
-                return new Lion();
-            else
-                return new Wolf();
-        }
-
-        //method to create Wildebeest or Bison in Africa
-        public override Herbivore CreateHerbivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection % 2 == 0)
-                return new Wildebeest();
-            else
-                return new Bison();
-        }
-    }
-
-    // North America is the Factory Class, which should has its own flora and fauna
-    public class NorthAmerica : Continent
-    {
-        //constructor to initialize list of carnivores and herbivores
-        public NorthAmerica()
-        {
-            herbivores = new List<Herbivore>();
-            carnivores = new List<Carnivore>();
-        }
-        //method to create Lion or Wolf in North America
-        public override Carnivore CreateCarnivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection % 2 == 0)
-                return new Lion();
-            else
-                return new Wolf();
-        }
-
-        //method to create Wildebeest in North America
-        public override Herbivore CreateHerbivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection % 2 == 0)
-                return new Wildebeest();
-            else
-                return new Bison();
-        }
-    }
-
     //client class
     public class AnimalWorld
     {
@@ -115,37 +45,6 @@ namespace Abstract_Factory
         }
     }
 
-    // Eurasia is the Factory Class, which should has its own flora and fauna
-    public class Eurasia : Continent
-    {
-        //constructor to initialize list of carnivores and herbivores
-        public Eurasia()
-        {
-            herbivores = new List<Herbivore>();
-            carnivores = new List<Carnivore>();
-        }
-        //method to create Lion or Wolf in Eurasia
-        public override Carnivore CreateCarnivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection % 2 == 0)
-                return new Tiger();
-            else
-                return new Tiger();
-        }
-
-        //method to create Wildebeest in Eurasia
-        public override Herbivore CreateHerbivore()
-        {
-            var rnd = new Random(DateTime.Now.Second);
-            var selection = rnd.Next(0, 100);
-            if (selection % 2 == 0)
-                return new Elk();
-            else
-                return new Elk();
-        }
-    }
     class Program
     {
         static void Main(string[] args)
