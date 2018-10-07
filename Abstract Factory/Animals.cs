0cc2e0fb26c5cfed7518be345c8fe5774fbcd9e2
Farid Huseynov <F.H189@hotmail.com>
2018-10-07 22:54:55 +0400

    Animals created inheriting abstract classes Herbivore and Carnivore

---
+++ Abstract Factory/Animals.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    class Animals
+    {
+        public abstract class Herbivore
+        {
+            public int Weight;
+            public abstract void EatGrass();
+            public bool Life; 
+        }
+        public abstract class Carnivore
+        {
+            public int Power;
+            public abstract void EatHerbivore(Herbivore herbivore);
+        }
+        public class Wildebeest : Herbivore
+        {
+            Wildebeest()
+            {
+                Weight = 50;
+                Life = true;
+            }
+            public override void EatGrass()
+            {
+                Weight += 10;
+            }
+        }
+        public class Bison : Herbivore
+        {
+            Bison()
+            {
+                Weight = 50;
+                Life = true;
+            }
+            public override void EatGrass()
+            {
+                Weight += 20;
+            }
+        }
+        public class Lion : Carnivore
+        {
+            Lion()
+            {
+                Power = 100;
+            }
+            public override void EatHerbivore(Herbivore herbivore)
+            {
+                if (herbivore.Life != false)
+                {
+                    if (Power > herbivore.Weight)
+                    {
+                        Power += 10;
+                        herbivore.Weight -= 10;
+                        if (herbivore.Weight == 0)
+                        {
+                            herbivore.Life = false;
+                        }
+                    }
+                    else
+                    {
+                        if (Power != 0)
+                        {
+                            Power -= 10;
+                        }
+                    }
+                }
+            }
+        }
+        public class Wolf : Carnivore
+        {
+            Wolf()
+            {
+                Power = 100;
+            }
+            public override void EatHerbivore(Herbivore herbivore)
+            {
+                if (herbivore.Life != false)
+                {
+                    if (Power > herbivore.Weight)
+                    {
+                        Power += 10;
+                        herbivore.Weight -= 10;
+                        if (herbivore.Weight == 0)
+                        {
+                            herbivore.Life = false;
+                        }
+                    }
+                    else
+                    {
+                        if (Power != 0)
+                        {
+                            Power -= 10;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
